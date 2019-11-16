54c8aecc777dbba935bd341cdfaa1ae77591062a
ryanscott906 <rmscott456@gmail.com>
2019-11-16 13:42:49 -0600

    updated index.html

--- script.js
+++ script.js
@@ -15,14 +15,31 @@ var FacebookMarketCap = $("#FacebookMarketCap");
 var FacebookCrypto = $("#FacebookCryptoVal");
 var cryptoValue = 
 //Apple Call And Append
-function() {
+function AppleDisplay() {
+    var querySymbol = "AAPL";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
     $.ajax({
-      url: ,
-      method: "GET"
-    })
+        url: AVurl,
+        method: "GET"
+    }).then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+    });
+    $.ajax({
+        url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
+        method: "GET"
+    }).then(function(response){
+        convRate = response.rate;
+        cryptoConvert();
+    });
+
     .then(function(response) {
         console.log(response);
-        $("#AppleDollarVal").text(response.price);
+        $("#AppleDollarVal").text(stockPrice);
         $("#AppleMarketCap").text(response.cap);
         $("#AppleCryptoVal").text(response.cryptoValue);
     
+
+function cryptoConvert() {
+    newPrice = stockPrice / convRate;
+    console.log(newPrice);
+    AppleDisplay();
