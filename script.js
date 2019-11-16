86ae73569b5aa69b79091d51d8658cae81953af5
MFarrell242 <farrell.michael.w@gmail.com>
2019-11-16 12:38:22 -0600

    converts currency now

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ $(document).ready(function(){
         url: AVurl,
         method: "GET"
     }).then(function(response){
-        console.log(response["Global Quote"]["05. price"]);
+        stockPrice = response["Global Quote"]["05. price"];
     });
     $.ajax({
         url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
         method: "GET"
     }).then(function(response){
-        console.log(response.rate);
+        convRate = response.rate;
+        cryptoConvert();
     });
+
 });
+
+function cryptoConvert() {
+    newPrice = stockPrice / convRate;
+    console.log(newPrice);
+}
