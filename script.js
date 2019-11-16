57a2c9ba69eafd1ad951840593a005462077aa66
MFarrell242 <farrell.michael.w@gmail.com>
2019-11-16 11:57:07 -0600

    stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+var querySymbol = "AAPL";
+
+$(document).ready(function(){
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response["Global Quote"]["05. price"]);
+    });
+    $.ajax({
+        url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+});
