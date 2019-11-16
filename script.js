5e5e26dfe4df55782aa105d0a17e23f424cceb85
MFarrell242 <farrell.michael.w@gmail.com>
2019-11-16 12:16:48 -0600

    new stuff

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ $(document).ready(function(){
         url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        console.log(response.rate);
     });
 });
