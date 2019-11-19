aa36ab4d7c1e1959ff92c8e7145b2fa528ebe69b
MFarrell242 <farrell.michael.w@gmail.com>
2019-11-18 21:27:09 -0600

    blah

--- script.js
+++ script.js
@@ -3,41 +3,45 @@ var getPrice = document.querySelector(".red");
 
 function cryptoConvert(q, r) {
     newPrice = q / r;
-    console.log(newPrice);
-    prependous("buttsCorp")
+    prependous(compName);
 }
 
-$(getPrice).on("click", function(){
+$(getPrice).on("click", function () {
     querySymbol = $(document.querySelector("#search")).val();
     var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
     $.ajax({
         url: AVurl,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         stockPrice = response["Global Quote"]["05. price"];
-        console.log(stockPrice);
-        cryptoConvert(stockPrice, convRate);
+        $.ajax({
+            url: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK",
+            method: "GET"
+        }).then(function (response) {
+            compName = response.bestMatches["0"]["2. name"];
+            $.ajax({
+                url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+                method: "GET"
+            }).then(function (response) {
+                convRate = response.rate;
+                cryptoConvert(stockPrice, convRate)
+            });
+        })
     });
-    $.ajax({
-        url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
-        method: "GET"
-    }).then(function(response){
-        convRate = response.rate;
-        console.log(convRate);
-        
-    });
-    
+
+
 });
 
 
 
 function prependous(name) {
-    var table = document.querySelector("tbody");
     var newElement = $("<tr>");
     let rowSymbol = $("<td>").text(querySymbol);
-    $(table).prepend(newElement);
-    $(newElement).append(name);
+    $("tbody").prepend(newElement);
+    $(newElement).append($("<td>").text(name));
     $(newElement).append(rowSymbol);
-    $(newElement).append(stockPrice);
-    $(newElement).append(newPrice);
+    $(newElement).append($("<td>").text("$" + stockPrice));
+    $(newElement).append($("<td>").text(newPrice));
 }
+
+// api keys: DE8538D9-7264-4490-90FD-0ABB4E2B1908  C0947395-C774-4C99-9908-9B82CB91E1F6
