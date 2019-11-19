aae751b181769bd8fa42abef7339de4af988c8c3
ryanscott906 <rmscott456@gmail.com>
2019-11-18 21:29:30 -0600

    not current

--- script.js
+++ script.js
@@ -14,92 +14,137 @@ var FacebookPrice = $("#FacebookDollarVal");
 var FacebookVolume = $("#FacebookVolume");
 var FacebookCrypto = $("#FacebookCryptoVal");
 
-
 function AppleCall() {
-var querySymbol = "AAPL";
-var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-$.ajax({
-    url: AVurl,
-    method: "GET"
-})
-.then(function(response){
-    const stockPrice = response["Global Quote"]["05. price"];
-    var stockVolume = response["Global Quote"]["06. volume"];
-    ApplePrice.text(stockPrice);
-    AppleVolume.text(stockVolume);
-})
-$.ajax({
-        url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=DE8538D9-7264-4490-90FD-0ABB4E2B1908",
+    var querySymbol = "AAPL";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
         method: "GET"
     })
-.then(function(response){
+    .then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+        stockVolume = response["Global Quote"]["06. volume"];
+        ApplePrice.text(stockPrice);
+        AppleVolume.text(stockVolume);
+
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+            method: "GET"
+    })
+    .then(function(response){
         convRate = response.rate;
-        function cryptoConvert() {
-    var newPrice = stockPrice / convRate;
-    console.log(newPrice);
-    AppleCrypto.text(newPrice);
-    cryptoConvert();
-        }})}
+        newPrice = stockPrice / convRate;
+        AppleCrypto.text(newPrice);
+        })
+    })
+}
+AppleCall();
+
+function MicrosoftCall() {
+    var querySymbol = "MSFT";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    })
+    .then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+        stockVolume = response["Global Quote"]["06. volume"];
+        MicrosoftPrice.text(stockPrice);
+        MicrosoftVolume.text(stockVolume);
+
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+            method: "GET"
+    })
+    .then(function(response){
+        convRate = response.rate;
+        newPrice = stockPrice / convRate;
+        MicrosoftCrypto.text(newPrice);
+        })
+    })
+}
+MicrosoftCall();
+
+function AppleCall() {
+    var querySymbol = "AAPL";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    })
+    .then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+        stockVolume = response["Global Quote"]["06. volume"];
+        ApplePrice.text(stockPrice);
+        AppleVolume.text(stockVolume);
+
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+            method: "GET"
+    })
+    .then(function(response){
+        convRate = response.rate;
+        newPrice = stockPrice / convRate;
+        AppleCrypto.text(newPrice);
+        })
+    })
+}
+AppleCall();
+
+function AppleCall() {
+    var querySymbol = "AAPL";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    })
+    .then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+        stockVolume = response["Global Quote"]["06. volume"];
+        ApplePrice.text(stockPrice);
+        AppleVolume.text(stockVolume);
+
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+            method: "GET"
+    })
+    .then(function(response){
+        convRate = response.rate;
+        newPrice = stockPrice / convRate;
+        AppleCrypto.text(newPrice);
+        })
+    })
+}
+AppleCall();
+
+function AppleCall() {
+    var querySymbol = "AAPL";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    })
+    .then(function(response){
+        stockPrice = response["Global Quote"]["05. price"];
+        stockVolume = response["Global Quote"]["06. volume"];
+        ApplePrice.text(stockPrice);
+        AppleVolume.text(stockVolume);
+
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
+            method: "GET"
+    })
+    .then(function(response){
+        convRate = response.rate;
+        newPrice = stockPrice / convRate;
+        AppleCrypto.text(newPrice);
+        })
+    })
+}
 AppleCall();
 
 
 
-function MicrosoftCall() {
-var querySymbol = "MSFT";
-var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-$.ajax({
-    url: AVurl,
-    method: "GET"
-})
-.then(function(response){
-    var stockPrice = response["Global Quote"]["05. price"];
-    var stockVolume = response["Global Quote"]["06. volume"];
-    MicrosoftPrice.text(stockPrice);
-    MicrosoftVolume.text(stockVolume);
-})};
-MicrosoftCall();  
 
-function AmazonCall() {
-var querySymbol = "AMZN";
-var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-$.ajax({
-    url: AVurl,
-    method: "GET"
-})
-.then(function(response){
-    var stockPrice = response["Global Quote"]["05. price"];
-    var stockVolume = response["Global Quote"]["06. volume"];
-    AmazonPrice.text(stockPrice);
-    AmazonVolume.text(stockVolume);
-})};
-AmazonCall();
 
-function GoogleCall() {
-var querySymbol = "GOOGL";
-var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-$.ajax({
-    url: AVurl,
-    method: "GET"
-})
-.then(function(response){
-    var stockPrice = response["Global Quote"]["05. price"];
-    var stockVolume = response["Global Quote"]["06. volume"];
-    GooglePrice.text(stockPrice);
-    GoogleVolume.text(stockVolume);
-})};
-GoogleCall();
-
-function FacebookCall() {
-var querySymbol = "FB";
-var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-$.ajax({
-    url: AVurl,
-    method: "GET"
-})
-.then(function(response){
-    var stockPrice = response["Global Quote"]["05. price"];
-    var stockVolume = response["Global Quote"]["06. volume"];
-    FacebookPrice.text(stockPrice);
-    FacebookVolume.text(stockVolume);
-})};
-FacebookCall();
