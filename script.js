4f5e3808e544d8dfac2f56d9445b06b27de1688a
Justin Abreu <justinabreu92@gmail.com>
2019-11-20 20:50:45 -0600

    Fixed the API Calls, need working search bar that can convert company name to ticker

--- script.js
+++ script.js
@@ -15,35 +15,10 @@ var FacebookPrice = $("#FacebookDollarVal");
 var FacebookVolume = $("#FacebookVolume");
 var FacebookCrypto = $("#FacebookCryptoVal");
 
-//Call for Amazon info
-function AmazonCall() {
-    var querySymbol = "AMZN";
-    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + "&apikey=8R49FW9VLKSNE9JK";
-    $.ajax({
-        url: AVurl,
-        method: "GET"
-    })
-    .then(function(response){
-        var stockPrice = response["Global Quote"]["05. price"];
-        var stockVolume = response["Global Quote"]["06. volume"];
-        AmazonPrice.text("$" + stockPrice);
-        AmazonVolume.text(stockVolume + " " + "Shares Today");
-    
-    $.ajax({
-            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=9F1B5FB4-A270-44EA-97F3-969AB45E6F08",
-            method: "GET"
-    })
-    .then(function(response){
-        var convRate = response.rate;
-        var newPrice = stockPrice / convRate;
-        AmazonCrypto.text("₿" + newPrice);
-
-        });
-    })}
-        
-    AmazonCall();
-
-//Array that hold API Keys
+//Due to the limit of API call that we are allowed to make per minute
+//We have created an array of API keys that are pulled from randomly
+//This allows us to be able to keep the site functioning
+//Arrays that hold the API Keys
 var alphaAPI = ["8R49FW9VLKSNE9JK", "D7BDX98JEX4CMDGY", "7YQ6SGFLSAWG2PNY", "R1HO6T0SA0NNCUFX"]
 var coinAPI = ["C0947395-C774-4C99-9908-9B82CB91E1F6", "9F1B5FB4-A270-44EA-97F3-969AB45E6F08", "3E618E22-0BFE-4AE3-96DD-CE807B909C95"]
 
@@ -56,8 +31,38 @@ function randomCoin(coinAPI){
     return coinAPI[Math.floor(Math.random() * coinAPI.length)];
 };
 
+//Call for Amazon info
+function AmazonCall() {
+    var querySymbol = "AMZN";
+    var AVurl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + querySymbol + `&apikey= ${randomAlpha(alphaAPI)}`;
+    $.ajax({
+        url: AVurl,
+        method: "GET"
+    })
+    .then(function(response){
+        var stockPrice = response["Global Quote"]["05. price"];
+        var stockVolume = response["Global Quote"]["06. volume"];
+        AmazonPrice.text("$" + stockPrice);
+        AmazonVolume.text(stockVolume + " " + "Shares Today");
+    
+    $.ajax({
+            url: "https://rest.coinapi.io/v1/exchangerate/BTC/USD?" + `&apikey= ${randomCoin(coinAPI)}`,
+            method: "GET"
+    })
+    .then(function(response){
+        var convRate = response.rate;
+        var newPrice = stockPrice / convRate;
+        AmazonCrypto.text("₿" + newPrice);
+
+        });
+    })}
+        
+    AmazonCall();
+
+
+
 //Variable that grabs the search button
-var searchBtn = $(".red");
+var searchBtn = $("#searchBtn");
 //Function that handles the onclick search button
 $(searchBtn).on("click", function () {
     querySymbol = $(document.querySelector("#search")).val();
